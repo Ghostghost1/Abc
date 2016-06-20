7562f4a64d20793244b1b0d2528ff481f8b40560
Ghostghost1 <royalking111111@gmail.com>
2016-06-20 23:11:31 +0200

    Pętlę cz. 1

--- CalkulatorApp/Calc.cs
+++ CalkulatorApp/Calc.cs
@@ -23,4 +23,36 @@ namespace CalkulatorApp
             object token;// objekt, który odpowiada za symbol
             object prevToken = 'w';// poprzedni symbol
 
-            
+
+            // Pętla która segreguje symboly
+            do
+            {
+                token = GetToken(s, ref pos);
+                // żeby działalo coś takie: -4 => 0-4
+                if (token is char && prevToken is char && (char)prevToken = '(' && ((char)token == '+' || (char)token == '-'))
+                    Operands.Push(0);
+                //żeby działalo coś takie: 4√(5+4) => 4*√(5+4)
+                if (token is char && (char)token == '√' && (prevToken is double || (char)prevToken == ')'))
+                    Functions.Push('*');
+
+                // jeżeli jiczba
+                if (token is double)
+                {
+                    Operands.Push((double)token);
+                }
+                // jeżeli operacja lub nawiasy
+                else if (token is char)
+                {
+                    // jeżeli podano zamknięty nawias to obliczamy wszystko np: (3+5)*8 => 8*8
+                    if ((char)token == ')')
+                    {
+                        while (Functions.Count > 0 && Functions.Peek() != '(')
+                            PopFunction(Operands, Functions);
+                        Functions.Pop();// zniszcza "("
+
+                        //żeby działalo coś takie: √(3*3)*6 => 3*6
+                        if (Functions.Count() != 0 && Functions.Peek() == '√')
+                            PopFunction(Operands, Functions);
+                    }
+                    else
+                   
